330ae8ca65cebb15da585a635c57ed3a81803de6
matheusedivado <ms1501085@gmail.com>
2024-10-11 23:13:14 -0300

    Criando projetos com npx create-react-app e npm create vite@latest

---
+++ 1_INTRO/hello-world-com-vite/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Hello World com Vite! 11/10/2024</p>
+    </>
+  )
+}
+
+export default App
---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World em React! 11/10/2024</p>
+    </div>
+  );
+}
+
+export default App;
