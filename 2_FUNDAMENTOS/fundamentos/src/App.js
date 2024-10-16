1d46890a89280ac8e1d040ec87ea7698b56c7fc2
matheusedivado <ms1501085@gmail.com>
2024-10-16 18:09:39 -0300

    Importação de Componentes

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,9 +1,14 @@
+
+//Componentes
+import FirstComponent from "./components/FirstComponent";
+//Styles
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React!</h1>
+      <FirstComponent/>
     </div>
   );
 }
