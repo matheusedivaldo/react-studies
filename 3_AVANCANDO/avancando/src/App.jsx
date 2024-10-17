cafc3e97ea299476e807d935964a941ce1539276
matheusedivado <ms1501085@gmail.com>
2024-10-17 17:10:46 -0300

    Imagem em public

--- 3_AVANCANDO/avancando/src/App.jsx
+++ 3_AVANCANDO/avancando/src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <p>Avançando no React</p>
+      {/*Imagem em public*/}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
     </>
   )
 }
