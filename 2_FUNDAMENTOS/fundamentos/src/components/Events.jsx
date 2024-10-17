dcdbd4c54de481a05ad4a27a2c6ce60653d18a88
matheusedivado <ms1501085@gmail.com>
2024-10-17 15:40:57 -0300

    Funções de Renderização

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,24 +1,42 @@
 const Events = () => {
-
     const handleMyEvent = (e) => {
         console.log(e);
+        console.log("Ativou o evento!");
+    };
+
+    const renderSomething = (x) => {
+        if (x) {
+            return <h1>Renderizando isso!</h1>;
+        } else {
+            return <h1>Também posso renderizar isso!</h1>;
+        }
     };
 
     return (
         <div>
+            {/* evento */}
             <div>
                 <button onClick={handleMyEvent}>Clique aqui!</button>
             </div>
+            {/* lógica no evento */}
             <div>
-                <button onClick={() => console.log("Clicou!")}>Função direto do botão.</button>
-                <button onClick={() => {
-                    if (true) {
-                        console.log("Isso não deveria existir :).")
-                    }
-                }}>Maneira errada.</button>
+                <button onClick={() => console.log("Clicou!")}>
+                    Clique aqui também!
+                </button>
+                <button
+                    onClick={() => {
+                        if (true) {
+                            console.log("Isso não deve existir (;");
+                        }
+                    }}
+                >
+                    Clica aqui, por favor!
+                </button>
             </div>
+            {renderSomething(true)}
+            {renderSomething(false)}
         </div>
-    )
-}
+    );
+};
 
 export default Events;
