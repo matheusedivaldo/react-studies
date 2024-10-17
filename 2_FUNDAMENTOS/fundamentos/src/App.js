f46b433c862a137e4c865ba1df669577f01b192c
matheusedivado <ms1501085@gmail.com>
2024-10-17 15:26:20 -0300

    Eventos01

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,8 +1,8 @@
-
 //Componentes
 import FirstComponent from "./components/FirstComponent";
 import TemplateExpressions from "./components/TemplateExpressions";
 import MyComponent from "./components/MyCOmponent";
+import Events from "./components/Events";
 
 //Styles
 import "./App.css";
@@ -11,9 +11,10 @@ function App() {
   return (
     <div className="App">
       <h1>Fundamentos React!</h1>
-      <FirstComponent/>
-      <TemplateExpressions/>
-      <MyComponent/>
+      <FirstComponent />
+      <TemplateExpressions />
+      <MyComponent />
+      <Events />
     </div>
   );
 }
